3ff96bafe5f55f5723814662196720cccdec8d29
Sebastien Guillemin <seb@mrlem.org>
2019-09-03 15:36:12 +0200

    ui: setup ui

--- core/src/org/mrlem/origins/OriginsGame.kt
+++ core/src/org/mrlem/origins/OriginsGame.kt
@@ -3,30 +3,57 @@ package org.mrlem.origins
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.scenes.scene2d.InputEvent
+import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.utils.viewport.ScreenViewport
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
+
+// TODO - critical - scene: 3D viewport
+// TODO - critical - scene: more complex scene
+// TODO - medium - more complex ui / theme
 
 class OriginsGame : ApplicationAdapter() {
 
-    lateinit var batch: SpriteBatch
-    lateinit var img: Texture
+    lateinit var stage: Stage
+    lateinit var table: Table
 
     override fun create() {
-        batch = SpriteBatch()
-        img = Texture("badlogic.jpg")
+        stage = Stage(ScreenViewport())
+        Gdx.input.inputProcessor = stage
+
+        table = Table()
+        table.setFillParent(true)
+        stage.addActor(table)
+
+        val uiSkin = Skin(Gdx.files.internal("uiskin.json"))
+        val button1 = TextButton("Press me!", uiSkin)
+        button1.addListener(object : ClickListener() {
+            override fun clicked(event: InputEvent?, x: Float, y: Float) {
+                println("plop")
+            }
+        })
+        table.add(button1)
     }
 
     override fun render() {
-        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+        val delta = Gdx.graphics.deltaTime
+
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        batch.begin()
-        batch.draw(img, 0f, 0f)
-        batch.end()
+
+        stage.act(delta)
+        stage.draw()
+    }
+
+    override fun resize(width: Int, height: Int) {
+        stage.viewport.update(width, height, true)
     }
 
     override fun dispose() {
-        batch.dispose()
-        img.dispose()
+        stage.dispose()
     }
 
 }
